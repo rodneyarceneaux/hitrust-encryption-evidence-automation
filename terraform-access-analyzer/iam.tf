627c43f97b44d224948e449ccc36d2126457654e
rodneyarceneaux <96917619+rodneyarceneaux@users.noreply.github.com>
2025-12-02 15:45:52 -0500

    Update iam.tf

--- terraform-access-analyzer/iam.tf
+++ terraform-access-analyzer/iam.tf
@@ -1,33 +1,59 @@
-resource "aws_iam_role" "export_role" {
-  name = "iam-access-analyzer-export-role"
+resource "aws_iam_role" "lambda_role" {
+  name = "iam-access-analyzer-evidence-lambda-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
       Effect = "Allow"
       Principal = {
-        Service = "access-analyzer.amazonaws.com"
+        Service = "lambda.amazonaws.com"
       }
       Action = "sts:AssumeRole"
     }]
   })
 }
 
-resource "aws_iam_role_policy" "export_policy" {
-  name = "iam-access-analyzer-export-policy"
-  role = aws_iam_role.export_role.id
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "iam-access-analyzer-evidence-lambda-policy"
+  description = "Allow Lambda to list findings and write to S3 evidence bucket"
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "s3:PutObject",
-        "s3:GetBucketLocation"
-      ]
-      Resource = [
-        "${aws_s3_bucket.export_bucket.arn}/*"
-      ]
-    }]
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "access-analyzer:ListFindings",
+          "access-analyzer:GetFinding"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:AbortMultipartUpload",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.export_bucket.arn,
+          "${aws_s3_bucket.export_bucket.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
 }
