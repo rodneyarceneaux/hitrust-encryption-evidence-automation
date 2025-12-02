bd26b92b45d3c4ce9adace8d723c60c0bd379b0d
rodneyarceneaux <96917619+rodneyarceneaux@users.noreply.github.com>
2025-12-02 15:45:31 -0500

    Update s3.tf

--- terraform-access-analyzer/s3.tf
+++ terraform-access-analyzer/s3.tf
@@ -1,33 +1,13 @@
-resource "aws_s3_bucket" "export_bucket" {
-  bucket = var.export_bucket_name
-
-  force_destroy = false
-}
-
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.export_bucket.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
-  bucket = aws_s3_bucket.export_bucket.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
 resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
-  bucket = aws_s3_bucket.export_bucket.bucket
+  bucket = aws_s3_bucket.export_bucket.id
 
   rule {
     id     = "glacier-transition"
     status = "Enabled"
+
+    filter {
+      prefix = ""
+    }
 
     transition {
       days          = 30
@@ -35,3 +15,4 @@ resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
     }
   }
 }
+
