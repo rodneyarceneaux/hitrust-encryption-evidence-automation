3a38e5a364e2847ad3bac2e0da8f6def83ba90b5
rodneyarceneaux <96917619+rodneyarceneaux@users.noreply.github.com>
2025-12-02 15:48:34 -0500

    Update s3.tf

--- terraform-access-analyzer/s3.tf
+++ terraform-access-analyzer/s3.tf
@@ -1,3 +1,34 @@
+resource "aws_s3_bucket" "export_bucket" {
+  bucket        = var.export_bucket_name
+  force_destroy = false
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.export_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.export_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket                  = aws_s3_bucket.export_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
   bucket = aws_s3_bucket.export_bucket.id
 
